99e78d168d6c05eede68a2b9b9bbeeb9808c76e7
Zeeshan Akhtar <Zeeshan.akhtar@webable.digital>
2024-02-29 12:19:07 +0600

    First

---
+++ about.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Pricing example for Bootstrap</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="./assets/library/croppie/croppie.css" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Features</a>
+            <a class="p-2 text-dark" href="#">Enterprise</a>
+            <a class="p-2 text-dark" href="#">Support</a>
+            <a class="p-2 text-dark" href="#">Pricing</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Sign up</a>
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Techno Fest</h1>
+        <!-- <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p> -->
+    </div>
+
+    <div class="container-fluid">
+        <div class="card-deck mb-3 text-center ml-2 mr-2">
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Form</h4>
+                </div>
+                <div class="card-body text-left">
+                    <form>
+                        <div class="form-group">
+                            <label for="field_name">Name</label>
+                            <input type="text" class="form-control" id="field_name" name="field_name" placeholder="Enter Full Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="field_phone">Phone</label>
+                            <input type="text" class="form-control" id="field_phone" name="field_phone" placeholder="Enter Phone Number">
+                        </div>
+                        <div class="form-group">
+                            <label for="field_email">Email address</label>
+                            <input type="email" class="form-control" id="field_email" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="upload-demo">Upload Image</label>
+                            <input type='file' id="imgInp" />
+                        </div>
+                        <button type="submit" class="btn btn-primary float-right">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Crop</h4>
+                </div>
+                <div class="card-body">
+                    <img id="my-image" src="#" />
+                </div>
+                <div class="card-footer">
+                    <button id="use">Upload</button>
+                </div>
+            </div>
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Result</h4>
+                </div>
+                <div class="card-body">
+                    <img id="result" src="">
+                </div>
+                <div class="card-footer">
+                    <a id="download_image_a" href="/" download="myimage">Download >></a>
+                </div>
+            </div>
+        </div>
+
+        <!-- <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                    <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Features</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Cool stuff</a></li>
+                        <li><a class="text-muted" href="#">Random feature</a></li>
+                        <li><a class="text-muted" href="#">Team feature</a></li>
+                        <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                        <li><a class="text-muted" href="#">Another one</a></li>
+                        <li><a class="text-muted" href="#">Last time</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Resources</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Resource</a></li>
+                        <li><a class="text-muted" href="#">Resource name</a></li>
+                        <li><a class="text-muted" href="#">Another resource</a></li>
+                        <li><a class="text-muted" href="#">Final resource</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Team</a></li>
+                        <li><a class="text-muted" href="#">Locations</a></li>
+                        <li><a class="text-muted" href="#">Privacy</a></li>
+                        <li><a class="text-muted" href="#">Terms</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer> -->
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+     -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="./assets/library/croppie/croppie.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            console.log("ready!");
+        });
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                // $('#my-image').croppie('destroy');
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#my-image').attr('src', e.target.result);
+                    var resize = new Croppie($('#my-image')[0], {
+                        viewport: {
+                            width: 200,
+                            height: 200,
+                            type: 'circle'
+                        },
+                        boundary: {
+                            width: 300,
+                            height: 300
+                        },
+                        // showZoomer: false,
+                        // enableResize: true,
+                        enableOrientation: true
+                    });
+                    $('#use').fadeIn();
+                    $('#use').on('click', function() {
+                        resize.result('base64').then(function(dataImg) {
+                            var data = [{
+                                image: dataImg
+                            }, {
+                                name: 'myimgage.jpg'
+                            }];
+                            // use ajax to send data to php
+                            $('#result').attr('src', dataImg);
+                            $('#download_image_a').attr('href', dataImg);
+                            // $('#download_image_img').attr('src', dataImg);
+                            // $('#imgInp').attr('value', dataImg);
+                        }).destroy();
+
+                        // resize.destroy();
+
+                    })
+
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#imgInp").change(function() {
+            $('#my-image').Croppie('destroy');
+            readURL(this);
+        });
+    </script>
+
+</body>
+
+</html>
